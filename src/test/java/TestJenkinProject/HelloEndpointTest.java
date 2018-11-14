05a0e9323e17b269b362223158ca54d44f5250a0
gtheonse <gtheonse@gmail.com>
2018-11-14 16:09:14 +0530

    unit test update

--- src/test/java/TestJenkinProject/HelloEndpointTest.java
+++ src/test/java/TestJenkinProject/HelloEndpointTest.java
@@ -31,7 +31,7 @@ public class HelloEndpointTest {
     @Test
     public void testHello() throws Exception {
         when().get("/").then()
-                .body(is("Hello World!"));
+                .body(is("Hello World! GONZALO THEONSE"));
     }
 
     @Test
